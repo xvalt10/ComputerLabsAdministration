f4064de0f8fa9381f6e4330dca9d6de977ba21e3
t.valkovic <t.valkovic@VALKOVIC-TOMAS.dir.svc.accenture.com>
2015-01-14 17:01:09 +0100

    added resource management managed bean

---
+++ ComputerLabsAdministration-war/src/java/managedBeans/ResourceManagedBean.java
@@ -0,0 +1,112 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package managedBeans;
+
+
+
+import entities.Classrooms;
+import entities.Hardware;
+import entities.InstalledSoftware;
+import entities.Software;
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import sessionBeans.ClassroomsFacade;
+import sessionBeans.HardwareFacade;
+import sessionBeans.InstalledSoftwareFacade;
+import sessionBeans.SoftwareFacade;
+
+/**
+ *
+ * @author t.valkovic
+ */
+@ManagedBean
+@SessionScoped
+public class ResourceManagedBean {
+    
+    @EJB
+    ClassroomsFacade classroomsFacade;
+    
+    @EJB
+    HardwareFacade hardwareFacade;
+    
+    @EJB
+    SoftwareFacade softwareFacade;
+    
+    @EJB
+    InstalledSoftwareFacade installedSoftwareFacade;
+    
+    private Classrooms classroom;
+    private Hardware hardware;
+    private Software software;
+    private InstalledSoftware softwareByComputer;
+
+    public Classrooms getClassroom() {
+        return classroom;
+    }
+
+    public void setClassroom(Classrooms classroom) {
+        this.classroom = classroom;
+    }
+
+    public Hardware getHardware() {
+        return hardware;
+    }
+
+    public void setHardware(Hardware hardware) {
+        this.hardware = hardware;
+    }
+
+    public Software getSoftware() {
+        return software;
+    }
+
+    public void setSoftware(Software software) {
+        this.software = software;
+    }
+
+    public InstalledSoftware getSoftwareByComputer() {
+        return softwareByComputer;
+    }
+
+    public void setSoftwareByComputer(InstalledSoftware softwareByComputer) {
+        this.softwareByComputer = softwareByComputer;
+    }
+    
+    
+    
+    @PostConstruct
+    public void init(){
+        classroom=new Classrooms();
+        hardware=new Hardware();
+        software=new Software();
+        softwareByComputer=new InstalledSoftware();
+    }
+    
+    private void addClassroom(){
+    classroomsFacade.create(classroom);
+    }
+    
+    private void addHardware(int classroomId){
+    hardware.setClassRoomId(classroomsFacade.find(classroomId));
+    hardwareFacade.create(hardware);
+    }
+    
+    public void addSoftware(){
+    softwareFacade.create(software);
+    }
+    
+    /**
+     * Creates a new instance of ResourceManagedBean
+     */
+    public ResourceManagedBean() {
+    }
+
+
+    
+    
+}
